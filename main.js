86c5275ce979b78c50a6c474d148de5debd9edec
DiGino <gsotongbe@gmail.com>
2021-02-08 16:54:57 +0000

    Updated

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-let save_button = document.getElementById("save_button");
+let save_button = document.querySelector("#save_button");
 let task_list = [];
 
 save_button.addEventListener("click", saveTasks);
 
 function saveTasks() {
-	let task = document.getElementById("task_form").value;
-	let display_task = document.getElementById("task_list");
-	
+	let task = document.querySelector("#task_form").value;
+	let display_task = document.querySelector("#task_list");
+
 	if( typeof task !== 'undefined' && task !== '' ) {
 		 task_list.push(task);
 	}
-	
-	let display_list = task_list.map(liElements); //Create a new array with task in li and span element
-		
+
+	display_list = task_list.map(liElements); //Crée un nouveau tableau en y appliquant la fonction suivante
+
 		function liElements(task) {
-			return '<li>' + '<span class="task">' + task + '</span></li>';
+			return '<li class="task">' + task + '<button class="done-button">Fait</button>' + '</li>';
 		}
-		
-	
-	display_task.innerHTML = display_list.join(" "); //join() for remove the comma separator of the array
+
+
+	display_task.innerHTML = display_list.join(" "); //join() pour créer un nouveau tableau de chaînes de caractères tout en supprimant la virgule
 }
+
+console.log(task_list);
+function doneTasks(index) {
+
+}
+
+let listed_task = document.querySelector("#task_list");
+listed_task.addEventListener("click", function(e){
+	if(e.target && e.target.matches("button")) {
+
+			console.log("It's works");
+
+	}
+});
